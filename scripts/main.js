8a97836fc92366e27d1cd14c40629d461a1df022
Reggie Kim <reggiekim@gmail.com>
2016-08-22 14:58:44 -0400

    add form and notes on commenting in jsx

--- scripts/main.js
+++ scripts/main.js
@@ -9,8 +9,15 @@ var ReactDOM = require("react-dom");
 var StorePicker = React.createClass({
 
   render: function() {
+    var name = "Reggie"
+    // dbl backslashes or backslash-star can be used for comments, just like in regular JS
     return (
-      <p>hello</p>
+      <form className="store-selector">
+        {/*when commenting in JSX you have to use string interprolation with curly braces then backslash-star */}
+        <h2>Please Enter A Store</h2>
+        <input type="text" ref="storeId" required />
+        <input type="submit" />
+      </form>
     )
   }
 });
