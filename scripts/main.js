1b8359705ef03e80effca190bcaadf64c4edf075
Reggie Kim <reggiekim@gmail.com>
2016-08-23 15:56:13 -0400

    add event listener to submit button and transition from root component to app component

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,7 @@ var ReactRouter = require("react-router");
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var Navigation = ReactRouter.Navigation;
+var History = ReactRouter.History;
 
 var createBrowserHistory = require("history/lib/createBrowserHistory");
 
@@ -80,17 +81,28 @@ var Inventory = React.createClass({
 });
 
 
-
 /*
   StorePicker component
   This will let us make <StorePicker/>
 */
 var StorePicker = React.createClass({
+  mixins: [History],
+
+  goToStore: function(event) {
+    event.preventDefault();
+      //console.log("Clicked and submitted!");
+    // get the data from the input
+    var storeId = this.refs.storeId.value;
+      //console.log(storeId);
+
+    // transition from StorePicker component to App component
+    this.history.pushState(null, "/store/"+storeId);
+  },
 
   render: function() {
     // dbl backslashes or backslash-star can be used for comments, just like in regular JS
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         {/*when commenting in JSX you have to use string interprolation with curly braces then backslash-star */}
         <h2>Please Enter A Store</h2>
         <input type="text" ref="storeId" defaultValue={h.getFunName()} required />
@@ -99,6 +111,7 @@ var StorePicker = React.createClass({
     )
   }
 });
+
 
 /*
   Not Found Route
