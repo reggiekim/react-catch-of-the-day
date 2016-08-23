5fc9415ae558f7b03ff2195a647c3bbbef0f6b6c
Reggie Kim <reggiekim@gmail.com>
2016-08-23 17:53:32 -0400

    add form to inventory component

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ var ReactDOM = require("react-dom");
 var ReactRouter = require("react-router");
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
-var Navigation = ReactRouter.Navigation;
+//var Navigation = ReactRouter.Navigation; //now deprecated in react 1.0. use History
 var History = ReactRouter.History;
 
 var createBrowserHistory = require("history/lib/createBrowserHistory");
@@ -16,7 +16,23 @@ var h = require("./helpers");
 */
 var App = React.createClass({
 
-  render: function() {
+  getInitialState : function (){
+    return {
+      fishes : {},
+      order : {}
+    }
+  },
+
+  addFish : function(fish) {
+    var timestamp = (new Date()).getTime();
+    //update the state object
+    this.state.fishes['fish-'+timestamp] = fish;
+
+    //set the state
+    this.setState({ fishes : this.state.fishes });
+  },
+
+  render : function() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
@@ -25,7 +41,50 @@ var App = React.createClass({
         <Order />
         <Inventory />
       </div>
-      )
+    )
+  }
+});
+
+
+/*
+  Add Fish Form
+  <AddFishForm />
+*/
+var AddFishForm = React.createClass({
+
+  createFish : function (event) {
+    //Stop the form from submitting
+    event.preventDefault();
+
+    //Take data from the form and create an object
+    var fish = {
+      name : this.refs.name.value,
+      price : this.refs.price.value,
+      status : this.refs.status.value,
+      desc : this.refs.desc.value,
+      image : this.refs.image.value
+    }
+
+    console.log(fish);
+
+    //Add the fish the App state
+  },
+
+  render : function () {
+    return (
+      //<p>Testing this is the fish form</p>
+      <form className="fish-edit" onSubmit={this.createFish}>
+        <input type="text" ref="name" placeholder="Fish Name" />
+        <input type="text" ref="price" placeholder="Fish Price" />
+        <select ref="status">
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <textarea type="text" ref="desc" placeholder="Description"></textarea>
+        <input type="text" ref="image" placeholder="URL to Image" />
+        <button type="submit">+ Add Item</button>
+      </form>
+    )
   }
 });
 
@@ -36,7 +95,7 @@ var App = React.createClass({
 */
 var Header = React.createClass({
 
-  render: function() {
+  render : function() {
     return (
       <header className="top">
         <h1>Catch
@@ -48,7 +107,7 @@ var Header = React.createClass({
         <h3 className="tagline"><span>{this.props.tagline}</span></h3>
         {/* 'this' refers to the Header function. 'this.props' is an object that refers to the properties the Header function, which has a tagline passed to it by the App component  */}
       </header>
-      )
+    )
   }
 });
 
@@ -59,10 +118,10 @@ var Header = React.createClass({
 */
 var Order = React.createClass({
 
-  render: function() {
+  render : function() {
     return (
       <p>Order</p>
-      )
+    )
   }
 });
 
@@ -73,10 +132,14 @@ var Order = React.createClass({
 */
 var Inventory = React.createClass({
 
-  render: function() {
+  render : function() {
     return (
-      <p>Inventory</p>
-      )
+      <div>
+        <h2>Inventory</h2>
+
+        <AddFishForm />
+      </div>
+    )
   }
 });
 
@@ -88,7 +151,7 @@ var Inventory = React.createClass({
 var StorePicker = React.createClass({
   mixins: [History],
 
-  goToStore: function(event) {
+  goToStore : function(event) {
     event.preventDefault();
       //console.log("Clicked and submitted!");
     // get the data from the input
@@ -99,7 +162,7 @@ var StorePicker = React.createClass({
     this.history.pushState(null, "/store/"+storeId);
   },
 
-  render: function() {
+  render : function() {
     // dbl backslashes or backslash-star can be used for comments, just like in regular JS
     return (
       <form className="store-selector" onSubmit={this.goToStore}>
@@ -118,7 +181,7 @@ var StorePicker = React.createClass({
 */
 var NotFound = React.createClass({
 
-  render: function() {
+  render : function() {
     return(
       <h1>Error, page not found!</h1>
     )
