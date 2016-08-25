62d3f10415816ddd0878041ff0b52ef6c1304d8a
Reggie Kim <reggiekim@gmail.com>
2016-08-25 15:24:14 -0400

    display fish count, name, calculated price in order component

--- scripts/main.js
+++ scripts/main.js
@@ -170,6 +170,24 @@ var Header = React.createClass({
 */
 var Order = React.createClass({
 
+  renderOrder : function(key) {
+    var fish = this.props.fishes[key];
+    var count = this.props.order[key];
+
+    if(!fish) {
+      return <li key={key}>Sorry, fish no longer available!</li>
+    }
+
+    return (
+      <li>
+        {count}lbs
+        {fish.name}
+        <span className="price">{h.formatPrice(count * fish.price)}</span>
+      </li>
+    )
+
+  },
+
   render : function() {
     var orderIds = Object.keys(this.props.order);
 
@@ -189,6 +207,7 @@ var Order = React.createClass({
       <div className="order-wrap">
         <h2 className="order-title">Your Order</h2>
         <ul className="order">
+          {orderIds.map(this.renderOrder)}
           <li className="total">
             <strong>Total:</strong>
             {h.formatPrice(total)}
